e0f4a3d32477a347664955adef7b1621e5646e61
pandezhao <981317572@qq.com>
2018-11-25 09:14:59 -0500

    solve out of bound peerBitfield 4

--- whz_Project/src/com/whz/Config.java
+++ whz_Project/src/com/whz/Config.java
@@ -2,6 +2,7 @@ package com.whz;
 
 import java.util.HashMap;
 
+
 public class Config {
 	public static int myID = 1001;
 	public static final int sPort = 8000;
@@ -9,7 +10,8 @@ public class Config {
 	public static int pieceNum = 70;
 	public static int FileSize = 1032;
 	public static int PieceSize = 100;
-	public static int bitFieldLength = (int) Math.ceil(((double)FileSize/(double)PieceSize));
+	//public static int bitFieldLength = (int) Math.ceil(((double)FileSize/(double)PieceSize));
+	public static int bitFieldLength = (int) Math.ceil(pieceNum/8);
 	public static int k = 1;
 	public static int optimistic_unchoking_interval = 2000;
 	public static int unchoking_interval = 1000;//second
